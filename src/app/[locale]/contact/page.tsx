a816f9b38d8c381b570cbbb285961e49c17cdd88
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-11-28 17:50:13 -0300

    Teste de cookies

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 import React from 'react';
 import ContactComponent from '@/components/ContactComponent';
+import { setRequestLocale } from 'next-intl/server';
+import { cookies } from 'next/headers';
 
 const Contact = async () => {
+  const allCookies = await cookies()
+  const locale = allCookies.get('NEXT_LOCALE')
+  setRequestLocale(locale as unknown as string)
   return (
     <ContactComponent />
   );
