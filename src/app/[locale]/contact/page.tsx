1bb81b9b604914e4635c7db206f06e9fd4ff330a
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-11-28 17:47:27 -0300

    Teste pra ver se agora vai

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,12 +1,8 @@
 "use client"
 import React from 'react';
-import { setRequestLocale } from 'next-intl/server';
 import ContactComponent from '@/components/ContactComponent';
 
-const Contact = async ({ params }: { params: Promise<{ locale: string }> }) => {
-  const { locale } = await params
-  setRequestLocale(locale);
-
+const Contact = async () => {
   return (
     <ContactComponent />
   );
